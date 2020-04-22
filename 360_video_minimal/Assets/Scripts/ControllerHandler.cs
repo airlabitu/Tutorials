ff6f6d5c5a5eea7172fe1974c1006f6244ef95af
Halfdan Hauch Jensen <halfdanhauchjensen@gmail.com>
2020-04-22 12:14:21 +0200

    Unity 360 video minimal example added

---
+++ 360_video_minimal/Assets/Scripts/ControllerHandler.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class ControllerHandler : MonoBehaviour
+{
+    public VideoPlayer videoOne;
+    Transform camPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoOne = GameObject.Find("Video Sphere 1").GetComponent<VideoPlayer>();
+
+        videoOne.Play();
+
+        camPosition = GameObject.Find("OVRCameraRig").transform;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger) || Input.GetKeyUp("down"))
+        {
+            if (videoOne.isPlaying) videoOne.Pause();
+            else if(videoOne.isPaused) videoOne.Play();
+        }
+    }
+}
